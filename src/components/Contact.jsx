6b3a6b3d45eee564a55b15185bdae282bcbacf53
sanjitxdutta <duttasanjit691@gmail.com>
2025-08-05 09:47:37 +0530

    Added Contact form layout and added subtle hover effect to button

---
+++ src/components/Contact.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+
+const Contact = () => {
+  return (
+    <div className='text-center p-6 py-20 lg:px-32 w-full overflow-hidden' id='Contact'>
+      <h1 className='text-2xl sm:text-4xl font-bold mb-2 text-center'>Contact With Us</h1>
+      <p className='text-gray-500 max-w-80 text-center mb-8 mx-auto'>
+        Ready to Make a Move? Let's Build Your Future Together
+      </p>
+
+      <form className='max-w-2xl mx-auto text-gray-600 pt-8'>
+        <div className='flex gap-4 flex-row'>
+          <div className='w-full md:w-1/2 text-left'>
+            <label className='block mb-2 font-medium'>Your Name</label>
+            <input
+              className='w-full border border-gray-300 rounded py-3 px-4'
+              type='text'
+              name='Name'
+              placeholder='Your Name'
+              required
+            />
+          </div>
+          <div className='w-full md:w-1/2 text-left'>
+            <label className='block mb-2 font-medium'>Your Email</label>
+            <input
+              className='w-full border border-gray-300 rounded py-3 px-4'
+              type='email'
+              name='Email'
+              placeholder='Your Email'
+              required
+            />
+          </div>
+        </div>
+
+        <div className='my-6 text-left'>
+          <label className='block mb-2 font-medium'>Message</label>
+          <textarea
+            className='w-full border border-gray-300 rounded py-3 px-4 h-48 resize-none'
+            name='Message'
+            placeholder='Message'
+            required
+          ></textarea>
+        </div>
+
+        <button className='bg-blue-600 text-white py-2 px-12 mb-10 rounded hover:bg-blue-700 transition'>
+          Send Message
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Contact;
