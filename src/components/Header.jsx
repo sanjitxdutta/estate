762a5f14b77de70f96ad20a5bd0a05aa35629076
sanjitxdutta <duttasanjit691@gmail.com>
2025-08-05 12:40:50 +0530

    Added animation to the About section

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,11 +7,11 @@ const Header = () => {
         <div className='relative min-h-screen mb-4 bg-cover bg-center w-full overflow-hidden' style={{ backgroundImage: "url('header_img.png')" }} id='Header'>
             <Navbar />
             <motion.div
-            initial={{opacity: 0, y:100}}
-            transition={{duration: 1.5}}
-            whileInView={{opacity: 1, y: 0}}
-            viewport={{once: true}}
-            className='flex items-center justify-center min-h-screen text-white text-center px-6 md:px-20 lg:px-32'>
+                initial={{ opacity: 0, y: 100 }}
+                transition={{ duration: 1.5 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                viewport={{ once: true }}
+                className='flex items-center justify-center min-h-screen text-white text-center px-6 md:px-20 lg:px-32'>
                 <div>
                     <h2 className='text-5xl sm:text-6xl md:text-[82px] inline-block max-w-3xl font-semibold pt-20'>Explore homes that fit your dreams</h2>
                     <div className='space-x-6 mt-16'>
